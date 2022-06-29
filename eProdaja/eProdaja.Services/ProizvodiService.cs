b9ff7cfdafcd08a853f01b9c3e6bc00023fbe5b3
AmelFazlic <80152955+AmelFazlic@users.noreply.github.com>
2022-06-29 12:12:47 +0200

    Upoznavanje sa clean arhitekturom

---
+++ eProdaja/eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+        public List<Proizvodi> ProizvodiList = new List<Proizvodi> { new Proizvodi { Id = 1, Name = "Televizor" }, new Proizvodi { Id = 2, Name = "Frizider" } };
+        public IEnumerable<Proizvodi> Get()
+        {
+            return ProizvodiList;
+        }
+
+        public Proizvodi GetById(int id)
+        {
+            Proizvodi proizvodi = ProizvodiList.FirstOrDefault(x => x.Id == id);
+            return proizvodi;
+        }
+    }
+}
---
+++ eProdaja/eProdaja/Controllers/ProizvodiController.cs
@@ -0,0 +1,28 @@
+﻿using eProdaja.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace eProdaja.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProizvodiController : Controller
+    {
+        private readonly IProizvodiService _proizvodiService;
+        public ProizvodiController(IProizvodiService proizvodiService)
+        {
+            _proizvodiService = proizvodiService;
+        }
+
+        [HttpGet]
+        public IEnumerable<Proizvodi> Get()
+        {
+            return _proizvodiService.Get();
+        }
+        [HttpGet("{ID}")]
+        public Proizvodi GetById(int ID)
+        {
+            return _proizvodiService.GetById(ID);
+        }
+        
+    }
+}
